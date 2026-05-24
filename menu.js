5a18a87413589bb50ef696137943c0babf2a9168
Elis <elisconstantinescu89@gmail.com>
2026-05-24 20:00:21 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,5 +1,10 @@
 const startBtn = document.getElementById("startBtn");
+const optionsBtn = document.getElementById("optionsBtn");
 
 startBtn.addEventListener("click", () => {
     window.location.href = "main.html";
+});
+
+optionsBtn.addEventListener("click", () => {
+    window.location.href = "options.html";
 });
